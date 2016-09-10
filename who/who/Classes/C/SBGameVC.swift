ddec675e4cd7dd0ad55891cc981c4068aa508c9f
StudyTime <2822459649@qq.com>
2016-09-10 14:58:26 +0800

    SBGameCell

--- who/who/Classes/C/SBGameVC.swift
+++ who/who/Classes/C/SBGameVC.swift
@@ -243,3 +243,48 @@ class SBGameVC: UITableViewController, UIAlertViewDelegate, SBWordDisplayVCDeleg
     
     
 }
+
+
+class SBGameCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var msgBtn: SBCircleBtn!
+    
+    
+    var player : SBPlayer? {
+        
+        
+        get{
+            return self.player
+        }
+        
+        
+        set{
+            
+            func setupPlayer() {
+                nameLabel.text = newValue?.name
+                msgBtn.hidden = (newValue?.isRead)!
+            }
+            
+            setupPlayer()
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    
+    
+}
+
