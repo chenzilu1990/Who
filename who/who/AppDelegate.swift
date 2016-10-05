ab6821c0d7f6a7b6428665e018ce77fcb8b8e175
StudyTime <2822459649@qq.com>
2016-10-06 03:20:23 +0800

    我饿挼

--- who/who/AppDelegate.swift
+++ who/who/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        let a = 1
+        
+        
         window = UIWindow.init(frame: UIScreen.main.bounds)
         
         SBPlayerListVC.initialize()
