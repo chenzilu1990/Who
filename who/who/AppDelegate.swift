55b41b04eb52e5b74bf1ac2db45714200f64ad83
StudyTime <2822459649@qq.com>
2016-10-06 03:22:47 +0800

    玩儿

--- who/who/AppDelegate.swift
+++ who/who/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let a = 1
+        print(a)
         
         
         window = UIWindow.init(frame: UIScreen.main.bounds)
